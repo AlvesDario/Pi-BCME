1724a09acf0c48602010b03bb5849efde6932db4
AlvesDario <dario.alves@fatec.sp.gov.br>
2019-03-03 19:28:53 -0300

    initial img

--- src/acme/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Marca, Carro, Pessoa, Publicacao, Aluguel
-
-admin.site.register(Marca)
-admin.site.register(Carro)
-admin.site.register(Pessoa)
-admin.site.register(Publicacao)
-admin.site.register(Aluguel)
--- src/acme/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class AcmeConfig(AppConfig):
-    name = 'acme'
