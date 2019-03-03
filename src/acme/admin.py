09206a4a39021c79e8e6bc291c87a4ec84fd754d
AlvesDario <dario.alves@fatec.sp.gov.br>
2019-03-03 16:37:14 -0300

    setup the django admin

--- src/acme/admin.py
+++ src/acme/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
-# from .models import Post
+from .models import Marca, Carro, Pessoa, Publicacao, Aluguel
 
-# admin.site.register(Post)
+admin.site.register(Marca)
+admin.site.register(Carro)
+admin.site.register(Pessoa)
+admin.site.register(Publicacao)
+admin.site.register(Aluguel)
